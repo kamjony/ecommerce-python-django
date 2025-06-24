569ce9f8cd1f5e6872a787588ead8a385b0e1f29
Md Kamrul Amin <ka@mortgage-magic.co.uk>
2025-06-24 19:00:25 +0600

    Created password reset views

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
   path('register', views.register, name='register'),
@@ -28,7 +30,21 @@ urlpatterns = [
 
   path('profile-management', views.profile_management, name='profile-management'),
 
-  path('delete-account', views.delete_account, name='delete-account')
+  path('delete-account', views.delete_account, name='delete-account'),
+
+  #Password management urls/views
+
+  # 1) Submit our email form
+  path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+
+  # 2) Success msg stating that a password reset email was sent
+  path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+  # 3) Password reset link
+  path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+  # 4) Password success msg stating that our password was reset
+  path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 
