fa6dfca62b6132083cfb2784c29dc7a99480e98b
Md Kamrul Amin <ka@mortgage-magic.co.uk>
2025-07-13 18:23:07 +0600

    Added payment app and configured urls

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'store', # Django app
     'cart', # Django app
     'account', # Django app
+    'payment', # Django app
     'mathfilters', #Mathfilter package
     'crispy_forms',
     'crispy_bootstrap5',
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     
     path('account/', include('account.urls')), #Account app
 
+    path('payment/', include('payment.urls')), #Payment app
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
